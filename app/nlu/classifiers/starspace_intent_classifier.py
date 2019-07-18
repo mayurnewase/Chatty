87e4d889685db750781be21072611fbc8a30a730
Mayur Newase <mayurnewase111@gmail.com>
2019-07-18 15:14:39 +0530

    Update starspace_intent_classifier.py

--- app/nlu/classifiers/starspace_intent_classifier.py
+++ app/nlu/classifiers/starspace_intent_classifier.py
@@ -110,7 +110,7 @@ class EmbeddingIntentClassifier:
         self.intent_placeholder = intent_placeholder
         self.embedding_placeholder = embedding_placeholder
         self.similarity_op = similarity_op
-        self.nlp = spacy.load('en')
+        self.nlp = spacy.load('en_core_web_sm')
         self.vect = vectorizer
         self.use_word_vectors = use_word_vectors
 

cc3e18358dad0ab1177092a727aa97cadc8a5127
Mayur Newase <mayurnewase111@gmail.com>
2019-07-18 13:54:22 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,50 +8,50 @@ manager = Manager(app)
 
 @manager.command
 def install_nltk_dependencies():
-    # Downloading necessary NLTK datasets
-    from nltk import download
-    download("stopwords")
-    download("wordnet")
-    download('averaged_perceptron_tagger')
-    download('punkt')
-    print ("Done")
+	# Downloading necessary NLTK datasets
+	from nltk import download
+	download("stopwords")
+	download("wordnet")
+	download('averaged_perceptron_tagger')
+	download('punkt')
+	print ("Done")
 
 
 @manager.command
 def init():
-    retval = subprocess.call(["python", "-m", "spacy download en_core_web_sm])
-    #python -m spacy download en_core_web_sm
+	retval = subprocess.call(["python", "-m", "spacy download en_core_web_sm])
+	#python -m spacy download en_core_web_sm
 	sys.exit(retval)
-    
-    from app.agents.models import Bot
-    print("---inside init of manager---")
-    try:
-        # create default bot
-        bot = Bot()
-        bot.name = "default"
-        bot.save()
-        print("Created default bot")
-    except:
-        print("Default agent exists.. skipping..")
+	
+	from app.agents.models import Bot
+	print("---inside init of manager---")
+	try:
+		# create default bot
+		bot = Bot()
+		bot.name = "default"
+		bot.save()
+		print("Created default bot")
+	except:
+		print("Default agent exists.. skipping..")
 
 
-    # import some default intents
-    from app.intents.controllers import import_json
-    json_file = open("examples/default_intents.json", "r+")
-    stories = import_json(json_file)
-    print("Imported {} Stories".format(len(stories)))
+	# import some default intents
+	from app.intents.controllers import import_json
+	json_file = open("examples/default_intents.json", "r+")
+	stories = import_json(json_file)
+	print("Imported {} Stories".format(len(stories)))
 
-    try:
-        print("Training models..")
-        from app.nlu.tasks import train_models
-        train_models()
-        print("Training models finished..")
-    except Exception as e:
-        e = str(e)
-        if e == "NO_DATA":
-            e = "load Data first into mongodb. Reffer Readme."
-        print("Could not train models..skipping.. (reason: {})".format(e))
+	try:
+		print("Training models..")
+		from app.nlu.tasks import train_models
+		train_models()
+		print("Training models finished..")
+	except Exception as e:
+		e = str(e)
+		if e == "NO_DATA":
+			e = "load Data first into mongodb. Reffer Readme."
+		print("Could not train models..skipping.. (reason: {})".format(e))
 
 
 if __name__ == "__main__":
-    manager.run()
+	manager.run()

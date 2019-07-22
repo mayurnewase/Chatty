543ba0d250f3472c983c54ba1a858274e96db3d9
mayur <mayurnewase111@gmail.com>
2019-07-22 23:07:20 +0530

    added models in git

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-import os
-
 class Config(object):
     DEBUG = False
-    MONGODB_DB = "heroku_nl4m3cv8"
-    MONGODB_HOST = "ds253017.mlab.com"
-    MONGODB_PORT = 53017
-    MONGODB_USERNAME = "heroku_nl4m3cv8"
-    MONGODB_USERNAME = "mpu4itfan5g0kbh3605phbjeah"
+    MONGODB_DB = "iky-ai"
+    MONGODB_HOST = "127.0.0.1"
+    MONGODB_PORT = 27017
+    MONGODB_USERNAME = ""
+    MONGODB_USERNAME = ""
 
     # Intent Classifier model details
     MODELS_DIR = "model_files/"
@@ -22,10 +20,11 @@ class Development(Config):
 
 class Production(Config):
     # MongoDB Database Details
-    #MONGODB_DB = os.environ.get("MONGODB_URI")
-    MONGODB_SETTINGS = {
-        "host": os.environ.get("MONGODB_URI"),
-    }
+    MONGODB_DB = "iky-ai"
+    MONGODB_HOST = "mongodb"
+    MONGODB_PORT = 27017
+    MONGODB_USERNAME = ""
+    MONGODB_USERNAME = ""
 
     # Web Server details
     WEB_SERVER_PORT = 8001
